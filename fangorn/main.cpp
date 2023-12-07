2cd3a5ed66a415b256ce732e1abca9326f9319c5
3@t <user752@discord>
2023-12-07 19:42:20 +0100

    MAIN ROOT

---
+++ fangorn/main.cpp
@@ -0,0 +1,6 @@
+#include "main_functions.h"
+
+int main()
+{
+	function3at::hello();
+}
---
+++ fangorn/main_functions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+namespace function3at
+{
+	void hello()
+	{
+		std::cout <<
+			R"(
+********************
+*	Witaj serdecznie w projekcie fangorn;
+*	Poswiecony pacy nad git zarzadzaniem;
+*	Stworzmy pierwsza galaz!;
+
+	W trakcie tworzenia lokalnego repo, ktora docelowo wyslemy na zdalny serwer warto zastanowic sie na plikiem konfiguracyjnym '.gitignore';
+	Nasza pierwsza stworzona galezia bedzie 'clone_b';
+		---
+		| $ git checkout -b clone_b;
+		| $ git branch ignore_b && git checkout clone_b;
+		---
+		
+ 			Wiecej informacji na temat tworzenia galezi znajdziesz: "git branch"
+
+)";
+	}
+}
