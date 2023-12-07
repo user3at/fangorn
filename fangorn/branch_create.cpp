2b3ec65f1618b3b8f133ab814283e2d227629143
3@t <user752@discord>
2023-12-07 21:45:22 +0100

    branch_create created fine

---
+++ fangorn/branch_create.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+namespace function3at
+{
+
+    void branch_create()
+    {
+        std::cout <<
+            R"(
+******************** /
+*   II. [git branch] create && check
+*	git branch <name_of_branch>          == tworzy branch
+*
+                
+$ git show-branch                      == lista galezi;
+$ git branch -a                        == printuje wszystkie galezie(lokalne i zdalne);
+$ git branch -r                        == printuje galezie zdalne;
+$ git branch --merged                  == printuje zmergowane galezie;
+$ git branch --no-merged               == printuje niezmergowane;
+$ git branch -m <new_name>             == zmiana nazwy aktualnego branch'u;
+$ git branch -m <old_name> <new_name>  == zmiana nazwy z dowolnego miejsca;
+    ---
+    | git branch <name_of_branch>
+    ---
+
+                    Wiecej informacji jak zmienic aktualny branch znajdziesz: "git checkout";
+
+            I. Tworzenie galezi w oparciu o 'remote'
+            $ git checkout -b <remotebranch> origin/<remotebranch>
+
+
+)";
+    }
+}
