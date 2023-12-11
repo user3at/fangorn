03704acd20a6c129326fbf8e4264153bde05a958
3@t <user752@discord>
2023-12-11 13:17:58 +0100

    clean_dev ready to go

---
+++ fangorn/clean.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+namespace awiko
+{
+	void clean()
+	{
+		std::cout <<
+		R"(
+		********************
+		*
+		*
+	
+		)";
+	}
+}
---
+++ fangorn/clean.h
@@ -0,0 +1,5 @@
+#pragma once
+namespace awiko
+{
+	void clean();
+}
